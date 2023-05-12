1ba228b34f0799c8d364f79c4214dec715521d70
Eric Chen <eschen@cpp.edu>
2023-05-11 17:08:12 -0700

    add members list updating

--- Peer.java
+++ Peer.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import java.awt.event.*;
 class Peer{
@@ -8,8 +11,10 @@ class Peer{
     private DatagramSocket socket;
     private InetAddress address;
     private byte[] buf;
-    public HashMap<InetAddress, Integer> peers = new HashMap<InetAddress, Integer>();
+    public HashMap<InetSocketAddress, String> peers = new HashMap<InetSocketAddress, String>();
+    public ArrayList<InetSocketAddress> users = new ArrayList<InetSocketAddress>();
     private Gui gui;
+    private String username;
 
     public Peer() throws IOException{
         socket = new DatagramSocket();
@@ -22,15 +27,6 @@ class Peer{
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 1234);
         socket.send(packet);
 
-        //receive packet and return
-        packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
-        String received = new String(packet.getData(), 0, packet.getLength());
-        String[] peerStrings = received.split(",");
-        for(int i=0;i<peerStrings.length;i++){
-            peers.put(InetAddress.getByName(peerStrings[i].split(":")[0]),Integer.parseInt(peerStrings[i].split(":")[1]));
-        }
-
     }
 
     public void leaveChat() throws IOException {
@@ -40,34 +36,66 @@ class Peer{
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 1234);
         socket.send(packet);
     }
+
+    //Sends message to all connected users directly
+    public void sendMessage(String message){
+        for (InetSocketAddress key : this.users)
+        {
+            byte[] buffer = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, key.getAddress(), key.getPort());
+            try {
+                socket.send(packet);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+    }
+
+    //Format date value to MM/dd hh:mm
+    private static String dateFormat(String time){
+        long timestamp = Long.parseLong(time);
+        Date date = new Date(timestamp);
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd hh:mm a");
+        String formattedDate = formatter.format(date);
+        return formattedDate;
+    }
     
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Peer myClient = new Peer();
-        myClient.registerTracker("please send ip address list, thanks");
-        for (InetAddress key : myClient.peers.keySet())
-        {
-            System.out.println(key.getHostAddress()+":"+myClient.peers.get(key));
-
-        }
         new Thread(new PeerListener(myClient)).start();
         myClient.gui = new Gui();
-
+    
         // Add a listener for the Send button
         myClient.gui.addSendButtonListener(new ActionListener() {
+            private boolean isFirstMessage = true;
+    
             public void actionPerformed(ActionEvent e) {
                 try {
-                    String message = "MESSAGE:"+myClient.gui.getMessage();
-                    byte[] buffer = message.getBytes();
-                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, myClient.address, 1234);
-                    myClient.socket.send(packet);
-                    myClient.gui.clearMessage();
+                    //Send username to tracker if first message
+                    if (isFirstMessage) {
+                        myClient.username = myClient.gui.getMessage();  // Set the username from the first message
+                        myClient.registerTracker(myClient.gui.getMessage());
+                        isFirstMessage = false;
+                        myClient.gui.clearMessage();
+                        myClient.gui.addMessage("Username set as: " + myClient.username);
+                    //Check for discconnect message
+                    } else if (myClient.gui.getMessage().equals(".")) {
+                        myClient.leaveChat();
+                        System.exit(0);
+                    //Handle messages from user
+                    } else {
+                        String message = "MESSAGE:" + System.currentTimeMillis() +
+                            String.format("From %s: %s", myClient.username, myClient.gui.getMessage());
+                        myClient.sendMessage(message);
+                        myClient.gui.clearMessage();
+                    }
+                    
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
         });
-
     }
 
     //Listen for when tracker sends message
@@ -84,17 +112,36 @@ class Peer{
                     DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
                     peer.socket.receive(receivePacket);
                     String received = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                    peer.gui.addMessage(received);
-                    System.out.println(received);
+
+                    // Check for packet prefixes
+                    // Handle member list updating
+                    if (received.startsWith("CONNECT:")) {
+                        String peersList = received.substring("CONNECT:".length());
+                        System.out.println("Handling Connect");
+                        //receive packet and return
+                        String[] peerStrings = peersList.split(",");
+                        peer.users.clear();
+                        peer.peers.clear();
+                        for(int i=0;i<peerStrings.length;i++){
+                            InetSocketAddress ipPort = new InetSocketAddress(InetAddress.getByName(peerStrings[i].split(":")[0]),Integer.parseInt(peerStrings[i].split(":")[1]));
+                            peer.peers.put(ipPort,peerStrings[i].split(":")[2]);
+                            peer.users.add(ipPort);
+                        }
+                        peer.gui.setMembers(new ArrayList<>(peer.peers.values()));
+                    // Handle messages
+                    } else if (received.startsWith("MESSAGE:")) {
+                        String message = received.substring("MESSAGE:".length());
+                        String time = dateFormat(message.substring(0, 13));
+                        peer.gui.addMessage(time +" - "+ message.substring(13));
+                        System.out.println(received);
+                    } else {
+                        // Handle other cases or unrecognized messages
+                        System.out.println("Received unrecognized message: " + received);
+                    }
                 }
-                    
             } catch (Exception e) {
-                    System.out.println("Connection closed: " + e.getMessage());
-            } finally {
-                    
+                    e.printStackTrace();
             }
         }
     }
-
-
 }
--- Tracker.java
+++ Tracker.java
@@ -8,35 +8,49 @@ import java.io.*;
 public class Tracker {
 
     static DatagramSocket serverSocket;
-    public static HashMap<InetAddress, Integer> peers = new HashMap<InetAddress, Integer>();
+    // Hashmap Key is InetSocketAddress contains ip and port , value is username
+    public static HashMap<InetSocketAddress, String> peers = new HashMap<InetSocketAddress, String>();
 
 
-    public static void sendToAll(InetAddress ipAddress, int port){
+    public static void sendToAll(){
         
          //send list of clients in one string packet with the format "ip:port,ip:port,ip:port...etc"
          String[] peersArr= new String[peers.size()];
          int i=0;
-         for (InetAddress key : peers.keySet())
+         for (InetSocketAddress key : peers.keySet())
          {
-            peersArr[i]=key.getHostAddress()+":"+peers.get(key);
+            peersArr[i]=key.getAddress().getHostAddress()+":"+key.getPort()+":"+peers.get(key);
             i+=1;
          }
-         String message = Stream.of(peersArr).collect(Collectors.joining(","));
+         String message = "CONNECT:"+Stream.of(peersArr).collect(Collectors.joining(","));
 
          //send message client list
          try {
          byte[] buf;
          buf = message.getBytes();
          DatagramPacket packet;
-         for (InetAddress key : peers.keySet())
+         for (InetSocketAddress key : peers.keySet())
          {
-            packet = new DatagramPacket(buf, buf.length, key, peers.get(key));
+            packet = new DatagramPacket(buf, buf.length, key.getAddress(), key.getPort());
             serverSocket.send(packet);
          }
          } catch (IOException e) {
              System.err.println(e);
          }   
      
+    }
+
+    public static void sendMessage(String message){
+        for (InetSocketAddress key : peers.keySet())
+        {
+            byte[] buffer = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, key.getAddress(), key.getPort());
+            try {
+                serverSocket.send(packet);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
     }
     
     public static void main(String[] args) throws Exception{
@@ -53,31 +67,22 @@ public class Tracker {
             //get address and port from datagram and save to list
             InetAddress ipAddress = receivePacket.getAddress();
             int port = receivePacket.getPort();
-            String ip = ipAddress.toString();
-            if(!peers.containsKey(ipAddress)){
-                peers.put(ipAddress,port);        //saving client info to hashMap
-                sendToAll(ipAddress, port);
-            }
-
+            InetSocketAddress ipPort = new InetSocketAddress(ipAddress, port);
             String received = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            if(!peers.containsKey(ipPort)){
+                peers.put(ipPort,received);        //saving client info to hashMap
+                sendToAll();
+            }
 
             if(received.equals("disconnect")){
-                peers.remove(ipAddress);
-            } else if (received.startsWith("MESSAGE:")) {
-                // Message received from client
-                String message = received.substring("MESSAGE:".length());
-                System.out.println("Received message from " + ip + ":" + port + ": " + message);
-                byte[] buffer = message.getBytes();
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName("localhost"), peers.get(InetAddress.getByName("localhost")));
-                serverSocket.send(packet);
-                // Handle the message as needed
+                peers.remove(ipPort);
+                sendToAll();
             }
             
-
             //print out the current list of peers
-            for (InetAddress key : peers.keySet())
+            for (InetSocketAddress key : peers.keySet())
             {
-               System.out.println(key.getHostAddress()+":"+peers.get(key));
+               System.out.println(key.getAddress().getHostAddress()+":"+peers.get(key));
             }
 
         }
